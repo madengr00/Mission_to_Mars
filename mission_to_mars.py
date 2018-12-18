ab57645a1eeac1d21461f4f23976f21efa3e1e78
duelingthreads <duelingthreads@gmail.com>
2018-12-17 22:48:31 -0600

    adding app.py, html, all with basic functionality

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -5,11 +5,9 @@ from splinter.exceptions import ElementDoesNotExist
 import requests
 import pandas as pd
 
-def scrape():
-    # ## Step 1 - Scraping from multiple sites
+def scrape_info():
     executable_path = {'executable_path': 'chromedriver.exe'}
-    browser =  Browser('chrome', **executable_path, headless=False) 
-
+    browser =  Browser('chrome', **executable_path, headless=False)
     #a dictionary to story the scraped data
     mars_data = {}
 
@@ -28,7 +26,7 @@ def scrape():
     for story in news_story:
         news_title = story.find('div','content_title').text
         news_para = story.find('div','article_teaser_body').text
-        mars_data["news story"] = (news_title,news_para)
+        mars_data["news_story"] = (news_title,news_para)
         
         print(news_title)
         print(news_para)
@@ -87,9 +85,11 @@ def scrape():
     mars_html_table = mars_df.to_html()
     mars_html_table
         #Strip unwanted newlines (\n)
-    mars_html_table.replace('\n','')
-    final_mars_table = mars_df.to_html('mars_table.html')
+    mars_html_table = mars_html_table.replace('\n','')
+    final_mars_table = mars_html_table
     mars_data["mars_facts"] = (final_mars_table)
+    print(final_mars_table)
+    
 
 
     ################### Mars Hemispheres ##################
@@ -103,32 +103,43 @@ def scrape():
     products = soup.find('div', class_='collapsible results')
     hemisphere = products.find_all('h3')
 
+    titles_and_urls = []
     #Create empty lists to store url and title
-    image_url_list = []
-    title_list =[]
+    #image_url_list = []
+    #title_list =[]
 
     #Scrape the url and title, store in lists
     for record in hemisphere:
         try:
             #Capture the title
-            title_list.append(record)
+            #title_list.append(record)
+            title = record.text
             #Click on the link
             browser.click_link_by_partial_text('Enhanced')
             #find the Original Image link on the new page
             downloads = browser.find_link_by_text('Sample').first
-            link = downloads['href']
+            image_url = downloads['href']
             #Capture the sample image url
-            image_url_list.append(link)
+            #image_url_list.append(image_url)
+            title_and_url = {title:image_url}
+            #title_and_url = zip(title, image_url)
+            #title_and_url = set(title_and_url)
+            titles_and_urls.append(title_and_url)
+            print(title_and_url)
         except ElementDoesNotExist:
             print("Scraping Complete")
 
     # use zip() to map values
-    titles_and_urls = zip(title_list, image_url_list)
+    #titles_and_urls = zip(title_list, image_url_list)
     # convert values to print as a set
-    titles_and_urls = set(titles_and_urls)
+    #titles_and_urls = set(titles_and_urls)
     print(titles_and_urls)
     mars_data["mars_hemispheres"] = (titles_and_urls)
     
+    # Close the browser after scraping
+    browser.quit()
+
+    #Return mars data dictionary
     return(mars_data)
 
-print(scrape())
+print(scrape_info)
---
+++ mission_to_mars_flask.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+#import scraping .py file (returns dictionary)
+import mission_to_mars
+
+# Create the application instance
+app = Flask(__name__)
+
+#Use PyMongo to establish Mongo Connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+# Create a URL route in the application for "/"
+@app.route("/")
+def home():
+    #find one record of data from the mongo db
+    mars_data = mongo.db.mars_data.find_one()
+    #return template and data
+    return render_template("index.html", mars_data = mars_data)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scraper():
+    #Run the scrape function
+    mars_data = mongo.db.mars_data
+    mars_data_data = mission_to_mars.scrape()
+    #Update the Mongo database using update and insert=True)
+    mars_data.update({},mars_data_data,upsert=True)
+
+    #Redirect back to the home page
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
